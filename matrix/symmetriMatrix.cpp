53f509d08a1a6c59b94954fbb99395330ae16c29
Arego11 <aregar20012001@gmail.com>
2026-06-04 20:34:57 -0700

    wip

--- matrix/symmetriMatrix.cpp
+++ matrix/symmetriMatrix.cpp
@@ -3,59 +3,68 @@ using namespace std;
 
 class BottomDiagonal
 {
-	private:
-	int n;
-	int *A;
+private:
+    int n;
+    int *A;
 
-	public:
-	BottomDiagonal(int n)
-{
-	this->n = n;
-	A = new int[n];
-}
-
-    void set(int i, int x)
+public:
+    BottomDiagonal(int n)
     {
-        A[i-1] = x;
+        this->n = n;
+        A = new int[n];
     }
-
-    int get(int i, int j)
-    {
-        if(j <= i) return A[j-1];
-        else return A[i-1];
     
-    }
-
-    void Display()
-    {
-        for(int i = 0; i < n; i++)
-        {
-            for (int j = 0; j < n; j++)
-            {
-                cout << get(i,j);
-
-            } cout << endl;
-        }
-    }
-
-    ~BottomDiagonal()
-    {
-        delete[] A;
-    }
+    void set(int i, int x);
+    int get(int i, int j);
+    void Display();
+    ~BottomDiagonal();
 };
 
-
-int main() 
+void BottomDiagonal::set(int i, int x)
 {
-	BottomDiagonal d(5);
+    A[i] = x;
+}
 
-    d.set(0,1);
-    d.set(1,2);
-    d.set(2,3);
-    d.set(3,4);
-    d.set(4,5);
+int BottomDiagonal::get(int i, int j)
+{
+    if (j <= i)
+    {
+        return A[j];
+    }
+    else
+    {
+        return A[i];
+    }
+}
 
-	d.Display();
-	
-	return 0;
+void BottomDiagonal::Display()
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cout << get(i, j);
+        }
+        cout << endl;
+    }
+}
+
+BottomDiagonal::~BottomDiagonal()
+{
+    delete[] A;
+}
+
+int main()
+{
+    BottomDiagonal d(5);
+
+    d.set(0, 2);
+    d.set(1, 3);
+    d.set(2, 4);
+    d.set(3, 5);
+    d.set(4, 6);
+
+    d.Display();
+
+    return 0;
 }
